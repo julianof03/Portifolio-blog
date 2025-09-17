08dc151eb3e61c5bb81f3f49409dd6178bc67f3b
Juliano Filho <jlprod08@gmail.com>
2025-09-17 03:02:51 -0300

    feat(leftHolder): estrutura basica para leftHolder

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './index.css';
 import TopHolder from './components/topHolderFamily/topHolder.jsx';
+import LeftHolder from './components/leftHolderFamily/LeftHolder.jsx';
 
 function App() {
   return (
     <div>
       <TopHolder></TopHolder>
+      <LeftHolder></LeftHolder>
         <p>hello</p>
     </div>
   )
---
+++ src/components/leftHolderFamily/leftHolder.jsx
@@ -0,0 +1,14 @@
+import './leftHolder.css';
+import React from 'react'
+function LeftHolder() {
+  console.log('App está renderizando')
+
+  
+  return (
+    <div className='LeftHolder'>
+        <p>hello</p>
+    </div>
+  )
+}
+
+export default LeftHolder
